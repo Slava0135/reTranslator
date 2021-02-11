a2c639919329297a3b01be7a82901a51fe860ffd
slava0135 <super.novalskiy_0135@inbox.ru>
2021-02-11 21:04:33 +0300

    h

--- src/retranslator/world/blocks/power/ReTranslator.kt
+++ src/retranslator/world/blocks/power/ReTranslator.kt
@@ -1,16 +1,18 @@
 package retranslator.world.blocks.power
 
 import arc.Core
-import arc.func.Floatp
-import arc.func.Prov
+import arc.Core.atlas
+import arc.graphics.g2d.Draw
 import arc.graphics.g2d.Lines
+import arc.graphics.g2d.TextureRegion
 import arc.math.Mathf
 import arc.math.geom.Geometry
-import mindustry.Vars
+import arc.math.geom.Vec2
 import mindustry.Vars.tilesize
-import mindustry.Vars.world
+import mindustry.content.Fx
 import mindustry.core.UI
 import mindustry.gen.Building
+import mindustry.graphics.Drawf
 import mindustry.graphics.Pal
 import mindustry.ui.Bar
 import mindustry.world.blocks.power.PowerBlock
@@ -21,12 +23,21 @@ open class ReTranslator(name: String) : PowerBlock(name) {
 
     var range = 8f
     var power = 50f
+
+    var laser: TextureRegion? = null
+    var laserEnd: TextureRegion? = null
     
     init {
         consumesPower = false
         outputsPower = false
         canOverdrive = false
         drawDisabled = false
+    }
+
+    override fun load() {
+        super.load()
+        laser = atlas.find("laser")
+        laserEnd = atlas.find("laser-end")
     }
 
     override fun setBars() {
@@ -72,5 +83,21 @@ open class ReTranslator(name: String) : PowerBlock(name) {
     override fun setStats() {
         super.setStats()
         stats.add(Stat.powerRange, range, StatUnit.blocks)
+    }
+
+    inner class ReTranslatorBuild : Building() {
+
+        var target: ReTranslatorBuild? = null
+
+        override fun draw() {
+            super.draw()
+            target?.let {
+                Drawf.laser(team, laser, laserEnd, x, y, it.x, it.y)
+            }
+        }
+
+        override fun drawSelect() {
+            drawPlace(tileX(), tileY(), rotation, true)
+        }
     }
 }
