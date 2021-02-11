a736e327083cd93d8179e1c38460856138005195
slava0135 <super.novalskiy_0135@inbox.ru>
2021-02-11 20:43:28 +0300

    h

--- src/retranslator/content/ReBlocks.kt
+++ src/retranslator/content/ReBlocks.kt
@@ -1,13 +1,19 @@
 package retranslator.content
 
+import mindustry.content.Items
 import mindustry.ctype.ContentList
+import mindustry.type.Category
+import mindustry.type.ItemStack
 import mindustry.world.Block
 import retranslator.world.blocks.power.ReTranslator
 
 class ReBlocks : ContentList {
     override fun load() {
         retranslator = object : ReTranslator("retranslator") {
-
+            init {
+                requirements(Category.power, ItemStack.with(Items.copper, 1, Items.lead, 3))
+                alwaysUnlocked = true
+            }
         }
     }
 
